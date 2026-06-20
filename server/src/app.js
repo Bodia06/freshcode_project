b82668e1bfb66aae8a4a84736a46dff17796acb0
Bohdan <bohdan.verbetskyi@gmail.com>
2026-06-20 21:30:29 +0200

    Fix server links

--- server/src/app.js
+++ server/src/app.js
@@ -8,11 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || origin) {
-        callback(null, true);
-      }
-    },
+    origin: 'https://squad-help.vercel.app',
     credentials: true,
   })
 );
